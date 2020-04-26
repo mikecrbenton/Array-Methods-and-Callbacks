cc07ec1160b76f8463e20fbfbc239c0192cecad0
mikecrbenton <mikecrbenton@hotmail.com>
2020-04-25 19:08:53 -0500

    finished 2 - don't understand 1

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 import { fifaData } from './fifa.js';
-console.log(fifaData);
+//console.log(fifaData);
 
 
 // ⚽️ M  V P ⚽️ //
 
-/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
+// Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
 
-(a) Home Team name for 2014 world cup final
-(b) Away Team name for 2014 world cup final
-(c) Home Team goals for 2014 world cup final
-(d) Away Team goals for 2014 world cup final
-(e) Winner of 2014 world cup final */
+// (a) Home Team name for 2014 world cup final
+    //console.log(fifaData);
+// (b) Away Team name for 2014 world cup final
+// (c) Home Team goals for 2014 world cup final
+// (d) Away Team goals for 2014 world cup final
+// (e) Winner of 2014 world cup final 
 
 
-/* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
+/* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of 
+objects with only finals data */
 
-function getFinals(/* code here */) {
+function getFinals( data ) {
 
-    /* code here */
-
+    const finalsArray = data.filter( item => item.Stage === "Final" );
+    //git console.log(finalsArray);
+    return finalsArray;
 };
+getFinals(fifaData);
+
 
 /* Task 3: Impliment a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
